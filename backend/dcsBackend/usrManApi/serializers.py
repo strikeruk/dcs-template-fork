c603f9d7cb633742f79d10e2e886b1f2db6440a1
Maria-Docs-Refrence-GL <lolitamascarenhas@gmail.com>
2024-09-15 17:30:35 +0530

    Update serializers.py

--- backend/dcsBackend/usrManApi/serializers.py
+++ backend/dcsBackend/usrManApi/serializers.py
@@ -1,1 +1,20 @@
+from rest_framework import serializers
+from .models import CustomUser, UserProfile
+
+## Serializer for the UserProfile model
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['phone_number', 'address', 'date_of_birth']
+
+
+ # Serializer for the CustomUser model       
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'profile']
 
